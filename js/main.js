1cac780ff04a9abc655474aaeb01daea6b12e480
Vanapalli Naveen Kumar <naveen.vanapalli@forsys.taas.com>
2019-11-24 22:51:28 +0530

    Added Json file and js file

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var myCat = {
+    "name":"Jerry",
+    "species":"Cat",
+    "favFood":"Milk"
+}
+var thePets=[
+    {
+        "name":"Jerry",
+        "species":"Cat",
+        "favFood":"Milk"
+    },{
+        "name":"Barky",
+        "species":"Dog",
+        "favFood":"Meat"
+    }
+]
